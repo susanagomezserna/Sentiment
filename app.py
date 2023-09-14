d5338639e08ccbbcd18d98546b56c7544c52e845
susanagomezserna <143086959+susanagomezserna@users.noreply.github.com>
2023-09-14 06:22:16 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from textblob import TextBlob
+import pandas as pd
+import streamlit as st
+
+from googletrans import Translator
+
+translator = Translator()
+st.header('Análisis de Sentimiento')
+with st.expander('Analizar texto'):
+    text = st.text_input('Escribe por favor: ')
+    if text:
+
+        translation = translator.translate(text, src="es", dest="en")
+        trans_text = translation.text
+        blob = TextBlob(trans_text)
+        st.write('Polarity: ', round(blob.sentiment.polarity,2))
+        st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
+        x=round(blob.sentiment.polarity,2)
+        if x >= 0.5:
+            st.write( 'Es un sentimiento Positivo 😊')
+        elif x <= -0.5:
+            st.write( 'Es un sentimiento Negativo 😔')
+        else:
+            st.write( 'Es un sentimiento Neutral 😐')
